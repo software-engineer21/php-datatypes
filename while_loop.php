7a5bcf08c0710b980bb692f868418bc01c3a8099
Tanvir-web-developer <tanvirtanuu@hotmail.com>
2021-06-30 22:33:55 +0600

    while loop in PHP

---
+++ while_loop.php
@@ -0,0 +1,29 @@
+<!-- <?php 
+
+	$i = 1;
+	while($i < 100) {
+		$i = $i + 1;
+		echo $i.'<br>';
+	}
+
+?> -->
+<!-- 
+<?php 
+
+	$a = 100;
+	while ( $a > 1) {
+		$a = $a - 1;
+		echo $a.'<br>';
+	}
+
+ ?> -->
+
+ <?php 
+
+ $a = 10;
+ while ($a < 20) {
+ 	$a = $a + 1;
+ 	echo ' <li> ' .'We Love PHP.' . '</li>';
+ }
+
+  ?>
