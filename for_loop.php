c03a41ba1bb23d48fd031c747cf0f3b724ba5cd0
Tanvir-web-developer <tanvirtanuu@hotmail.com>
2021-07-01 01:09:22 +0600

    for loop in PHP

---
+++ for_loop.php
@@ -0,0 +1,20 @@
+<?php 
+
+	for($i=0; $i <=100; $i= $i+5):
+		echo $i.'<br>';
+	
+		endfor;
+
+
+		echo "<br>";
+ ?>
+
+
+<?php 
+
+	for($i=100; $i >=0; $i = $i - 5):
+		echo $i.'<br>';
+	endfor;
+
+
+ ?>
