8d8d5bcd6934a4bd564470a4582024e143b33125
Tanvir-web-developer <tanvirtanuu@hotmail.com>
2021-07-01 01:41:50 +0600

    Indexed Array in PHP

---
+++ indexed_array.php
@@ -0,0 +1,11 @@
+<?php 
+
+	// $names = ['Tanvir', 'Tamim', 'Thowhid', 'Tarek'];
+
+	// echo $names[0].'<br>';
+
+ 	$names = ['Tarek' , 'Thowhid' , 'Tanvir' , 'Tamim'];
+ 	for ( $i=0; $i<4; $i++) {
+ 		echo $names[$i].'<br>';
+ 	}
+
