a4ddfd6675891115d2f86da0f94b071aa49606e0
Tanvir-web-developer <tanvirtanuu@hotmail.com>
2021-07-01 02:15:20 +0600

    Associative array in PHP

---
+++ associative_array.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+	$items = array(
+		'tomato_price' => 75,
+		'lau_price' => 100,
+		'Alu_price' => 200,
+		'gajor' => 300
+	 );
+
+	echo "<pre>";
+	print_r($items);
+	echo "</pre>";
+
+
+
+
+ ?>
