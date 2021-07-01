be431b6eb19d795d13ec763691c1ad9ec5e402f1
Tanvir-web-developer <tanvirtanuu@hotmail.com>
2021-07-01 12:44:53 +0600

    foreach loop in PHP

---
+++ foreach_loop.php
@@ -0,0 +1,39 @@
+<?php 
+
+
+	$names = array(
+		'Tanvir' => 21,
+		'Shahidul' => 22,
+		'Mithu' => 23, 
+		'Ezzq' => 26
+	);
+
+	echo '<ul>';
+	foreach ($names as $key => $age) {
+		echo '<li> Name is: ' . $key . '. Age is: ' . $age . '</li>' . '<br>';
+	}
+
+	echo '</ul>';
+
+	?>
+
+<?php 
+
+
+
+	$infos = array(
+		'q' => 21,
+		'w' => 22,
+		'e' => 23
+	);
+
+	foreach ($infos as $info) {
+		echo $info . '<br>' ; 
+	}
+	
+
+
+
+
+
+ ?>
--- functions.php
+++ functions.php
@@ -6,3 +6,21 @@
 	}
 
 	details ('Vivo', 'Vivo v20', 'last month');
+
+
+
+
+	function addition ($a, $b) {
+		echo "<br> Addition is" . ($a + $b);
+	}
+
+	addition(50, 50);
+
+
+
+
+	function minus ($i, $p) {
+		echo " <br> Minus value is " . ($i - $p );
+	}
+
+	minus(100, 50);
